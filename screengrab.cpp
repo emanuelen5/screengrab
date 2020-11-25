db7dd780cfe0a06a45e432130a62ffb982610035
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-11-25 23:41:54 +0100

    Creating a window with hello world text

--- screengrab.cpp
+++ screengrab.cpp
@@ -2,6 +2,7 @@
 #include <X11/Xutil.h>
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ int main(void) {
 	cout << "Screengrab\n";
 	cout << "*********************\n\n";
 
+
+    XEvent event;
+    char *msg = "Hello, World!";
 	int s;
 
 	Display* display = XOpenDisplay(nullptr);
@@ -40,6 +44,34 @@ int main(void) {
 	cout << "\tformat: " << image->format << endl;
 	cout << "\toffset: " << image->xoffset << endl;
 	cout << "\tbpp-line: " << image->bytes_per_line << endl;
+
+	// Display the grabbed image
+    // create window
+    Window window = XCreateSimpleWindow(display, RootWindow(display, s), 10, 10, 200, 200, 1,
+                                 BlackPixel(display, s), WhitePixel(display, s));
+ 
+    // select kind of events we are interested in
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+ 
+    // map (show) the window
+    XMapWindow(display, window);
+ 
+    // event loop
+	cerr << "Press any key to close the window\n";
+    for (;;)
+    {
+        XNextEvent(display, &event);
+ 
+        // draw or redraw the window
+        if (event.type == Expose)
+        {
+            XFillRectangle(display, window, DefaultGC(display, s), 20, 20, 10, 10);
+            XDrawString(display, window, DefaultGC(display, s), 50, 50, msg, strlen(msg));
+        }
+        // exit on key press
+        if (event.type == KeyPress)
+            break;
+    }
 
 	XDestroyImage(image);
 	XCloseDisplay(display);
